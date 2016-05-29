35f141997d8d90fd2b34afdbc209eb8e14269032
geebz <alexandrveklich@gmail.com>
2016-05-29 15:02:16 +0300

    update

--- crypto_1.py
+++ crypto_1.py
@@ -2,10 +2,18 @@
 
 from Polinom_class import Polinom
 from feature import Profiler
+from functools import partial
+from multiprocessing import Pool, cpu_count
 
 
 def kord_func(array):
-    return zip(*array)
+    A = []
+    for func in zip(*array):
+        new_array = []
+        for k in func:
+            new_array.append(int(k))
+        A.append(new_array)
+    return A
 
 def scalar_mul(a,b):
     return sum(int(bit) for bit in bin(a & b)[2:])
@@ -53,9 +61,9 @@ def error_propagation(array):
         for i in range(Polinom.power):
             i_arr = []
             for elem in range(2**Polinom.power):
-                a = int(array[func][elem],2)
+                a = array[func][elem]
                 xe = elem ^ (1 << Polinom.power-1-i)
-                b = int(array[func][xe], 2)
+                b = array[func][xe]
                 value = a ^ b
                 i_arr.append(value)
             sum_i_arr = sum(i_arr)
@@ -73,27 +81,11 @@ def koef_error_propagation(array):
         func_array.append(vec_i_array)
     return func_array
 
-def koef_Zhegalkin(truth_table):
-    koef_zheg = []
-    for num, func in enumerate(truth_table):
-        array = []
-        for i in range(Polinom.power):
-            used = set()
-            for num in range(2 ** Polinom.power):
-                if num in used:
-                    continue
-                num_inv = num ^ (1 << (Polinom.power - 1 - i))
-                used.add(num_inv)
-                array[num_inv] = array[num]^array[num_inv]
-        koef_zheg.append(array)
-        return koef_zheg
-
-
-def calc_max_alg_degree(truth_table):
+def Alg_degree(truth_table):
     F_degree = 0
     result = ''
-    for num, func in enumerate(zip(*truth_table)):
-        A = list(func[:])
+    for num, func in enumerate(truth_table):
+        array = list(func[:])
         for i in range(Polinom.power):
             used = set()
             for elem in range(2 ** Polinom.power):
@@ -101,18 +93,51 @@ def calc_max_alg_degree(truth_table):
                     continue
                 elem_inv = elem ^ (1 << (Polinom.power - 1 - i))
                 used.add(elem_inv)
-                A[elem_inv] = A[elem] ^ A[elem_inv]
+                array[elem_inv] = array[elem] ^ array[elem_inv]
 
-        max_wt = 0
-        for number, elem in enumerate(A):
+        max_fd = 0
+        for number, elem in enumerate(array):
             if elem:
-                max_wt = max(max_wt, bin(number)[2:].count('1'))
-        result += 'f{} = {} \n'.format(num, max_wt)
-        F_degree = max(F_degree, max_wt)
+                max_fd = max(max_fd, bin(number)[2:].count('1'))
+        result += 'f{} = {} \n'.format(num, max_fd)
+        F_degree = max(F_degree, max_fd)
     result += 'F = {}'.format(F_degree)
     return result
 
+def avalanche_effect(array):
+    g=0
+    f=0
+    for num,func in enumerate(array):
+        for elem in func:
+            if elem != 2**(Polinom.power-1):
+                f=f+1
+                if elem!=Polinom.power * 2**(Polinom.power-1):
+                    g=g+1
+                    break
+    if f>0:
+        print 'F avelanche effect False'
+    else:
+        print 'F avelanche effect True'
 
+    if g>0:
+        print 'F avg. avelanche effect False'
+    else:
+        print 'F avg. avelanche effect True'
+
+def get_mdp(truth_table):
+    def _get_mdp(truth_table, a):
+        b = [0] * (2 ** Polinom.power)
+        for x in range(2 ** Polinom.power):
+            b[truth_table[x] ^ truth_table[x ^ a]] += 1
+        return max(b)
+
+    chunk_size = cpu_count() - 1
+
+    processes = Pool(chunk_size)
+    MDPes = processes.map(partial(_get_mdp, truth_table),
+                          range(1, 2 ** Polinom.power))
+
+    return '{}/2^{}'.format(max(MDPes), Polinom.power)
 
 with Profiler() as p:
     # f = open('results.txt','r+')
@@ -126,3 +151,7 @@ with Profiler() as p:
     # disb = disbalance(uolsh)
     # kor_imun = k_balance(uolsh)
     # non = nonlinearity(uolsh)
+    # b = error_propagation(c)
+    # avalanche_effect(b)
+
+    print get_mdp(truth_table)
